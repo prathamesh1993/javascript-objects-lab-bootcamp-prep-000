94e1eb707e21518f80ec9e5f68dc396f46cd817b
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-02 07:47:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, { [key]: value })
 }
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
